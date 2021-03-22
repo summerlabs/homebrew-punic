0dd238412bd426c704acbb9b6418f6178ddd6fb4
Johnson Cheung <johnson@shred.app>
2021-03-22 11:17:13 -0700

    updated version

--- Formula/punic.rb
+++ Formula/punic.rb
@@ -1,7 +1,7 @@
 class Punic < Formula
   desc ""
   homepage ""
-  url "https://github.com/summerlabs/punic/releases/download/0.0.1-alpha/punic.tar.gz"
+  url "https://github.com/summerlabs/punic/releases/download/0.0.2-alpha/punic.tar.gz"
   sha256 "28262a6306996ddf4174dea71087c7ae62751ecd5a613e18e6834df0aaf131f9"
   license ""
 
