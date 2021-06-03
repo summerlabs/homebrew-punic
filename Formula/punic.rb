d2ea4fa5e7d9e77d005105f4397b7c4cc7325103
Andrew Aquino <andrewaquino118@gmail.com>
2021-06-02 17:17:30 -0700

    updated version to 0.0.8

--- Formula/punic.rb
+++ Formula/punic.rb
@@ -1,8 +1,8 @@
 class Punic < Formula
   desc ""
   homepage ""
-  url "https://github.com/summerlabs/punic/releases/download/0.0.7-beta/punic.tar.gz"
-  sha256 "437d53b1e94efcc66e772c71555acc7721aeb9ffe8f3af6666a044d075275dbc"
+  url "https://github.com/summerlabs/punic/releases/download/0.0.8/punic.tar.gz"
+  sha256 "443200e50ad1689757b0d6043d052acb0a28dc1a171406ef7df27f24b954d57c"
   license ""
 
 
